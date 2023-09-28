842dc3f4469927c1db526d82e3991904aa1b4fd2
no.oneho <oneho.dev@gmail.com>
2023-09-29 03:35:16 +0900

    Init: rds init

---
+++ src/main/java/com/book/heejinbook/config/JasyptConfig.java
@@ -0,0 +1,32 @@
+package com.book.heejinbook.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+
+    @Value("${secret-key-source}")
+    private String secretKeySource;
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(secretKeySource); //암호화 사용 키
+        config.setAlgorithm("PBEWithMD5AndDES"); //암호화 알고리즘
+        config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
