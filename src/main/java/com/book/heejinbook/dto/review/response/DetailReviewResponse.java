9a76e09571d26280efab1d76d7098fccff50e0af
no.oneho <oneho.dev@gmail.com>
2023-09-30 00:22:05 +0900

    Feat: 리뷰 디테일 조회

---
+++ src/main/java/com/book/heejinbook/dto/review/response/DetailReviewResponse.java
@@ -0,0 +1,32 @@
+package com.book.heejinbook.dto.review.response;
+
+import com.book.heejinbook.entity.Review;
+import com.book.heejinbook.utils.DateUtils;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DetailReviewResponse {
+
+    private Long reviewId;
+    private String reviewAuthor;
+    private String reviewTitle;
+    private String reviewContents;
+    private String reviewCreatedAt;
+
+    public static DetailReviewResponse from(Review review) {
+        return DetailReviewResponse.builder()
+                .reviewId(review.getId())
+                .reviewAuthor(review.getUser().getNickname())
+                .reviewTitle(review.getTitle())
+                .reviewContents(review.getContents())
+                .reviewCreatedAt(DateUtils.convertToString(review.getCreatedAt()))
+                .build();
+    }
+
+}
