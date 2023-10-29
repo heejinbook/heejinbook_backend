93f9c150688c246b88b21d18228152a4a36a0796
no.oneho <oneho.dev@gmail.com>
2023-10-30 00:39:44 +0900

    Fix: 책 검색 시 인코딩 처리

--- src/main/java/com/book/heejinbook/error/domain/BookErrorCode.java
+++ src/main/java/com/book/heejinbook/error/domain/BookErrorCode.java
@@ -11,7 +11,8 @@ public enum BookErrorCode implements ErrorCode {
 
     NOT_FOUND_BOOK(HttpStatus.NOT_FOUND.value(), "책을 찾을 수 없습니다"),
     INVALID_DATE(HttpStatus.BAD_REQUEST.value(), "날짜 형식이 이상합니다"),
-    INVALID_SORT_TYPE(HttpStatus.BAD_REQUEST.value(), "정렬값을 다시 확인해주세요")
+    INVALID_SORT_TYPE(HttpStatus.BAD_REQUEST.value(), "정렬값을 다시 확인해주세요"),
+    FAILED_SEARCH_KEYWORD_DECODING(HttpStatus.BAD_REQUEST.value(), "검색어 디코딩이 실패했습니다.")
     ;
 
 
